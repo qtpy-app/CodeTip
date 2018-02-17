9f0d7b58b788c10fd814630fcca3b1e54d9ce409
Lin JH <625781186@qq.com>
2018-02-18 01:46:20 +0800

    UI_py

--- Ui_MainWindow.py
+++ Ui_MainWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'D:\pyPro\CodeTip - 副本\MainWindow.ui'
+# Form implementation generated from reading ui file 'D:\pyPro\CodeTip\MainWindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.2
 #
@@ -17,9 +17,20 @@ class Ui_MainWindow(object):
         self.centralWidget = QtWidgets.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralWidget)
-        self.gridLayout.setContentsMargins(4, 4, 4, 4)
+        self.gridLayout.setContentsMargins(4, 4, 4, 0)
         self.gridLayout.setHorizontalSpacing(4)
         self.gridLayout.setObjectName("gridLayout")
+        self.widget = QtWidgets.QWidget(self.centralWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Ignored)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.widget.sizePolicy().hasHeightForWidth())
+        self.widget.setSizePolicy(sizePolicy)
+        self.widget.setObjectName("widget")
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.widget)
+        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.gridLayout.addWidget(self.widget, 0, 0, 1, 1)
         self.stackedWidget = QtWidgets.QStackedWidget(self.centralWidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Ignored)
         sizePolicy.setHorizontalStretch(0)
@@ -42,17 +53,6 @@ class Ui_MainWindow(object):
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.stackedWidget.addWidget(self.page1)
         self.gridLayout.addWidget(self.stackedWidget, 0, 1, 1, 1)
-        self.widget = QtWidgets.QWidget(self.centralWidget)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Ignored)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.widget.sizePolicy().hasHeightForWidth())
-        self.widget.setSizePolicy(sizePolicy)
-        self.widget.setObjectName("widget")
-        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.widget)
-        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout_4.setObjectName("verticalLayout_4")
-        self.gridLayout.addWidget(self.widget, 0, 0, 1, 1)
         self.gridLayout.setColumnStretch(0, 1)
         self.gridLayout.setColumnStretch(1, 5)
         MainWindow.setCentralWidget(self.centralWidget)
@@ -61,8 +61,22 @@ class Ui_MainWindow(object):
         self.toolBar.setOrientation(QtCore.Qt.Horizontal)
         self.toolBar.setObjectName("toolBar")
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        self.statusBar = QtWidgets.QStatusBar(MainWindow)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.statusBar.sizePolicy().hasHeightForWidth())
+        self.statusBar.setSizePolicy(sizePolicy)
+        self.statusBar.setMinimumSize(QtCore.QSize(0, 23))
+        self.statusBar.setSizeIncrement(QtCore.QSize(0, 23))
+        self.statusBar.setObjectName("statusBar")
+        MainWindow.setStatusBar(self.statusBar)
         self.saveaction = QtWidgets.QAction(MainWindow)
         self.saveaction.setObjectName("saveaction")
+        self.action12 = QtWidgets.QAction(MainWindow)
+        self.action12.setObjectName("action12")
+        self.actionas = QtWidgets.QAction(MainWindow)
+        self.actionas.setObjectName("actionas")
 
         self.retranslateUi(MainWindow)
         self.stackedWidget.setCurrentIndex(0)
@@ -74,6 +88,8 @@ class Ui_MainWindow(object):
         self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
         self.saveaction.setText(_translate("MainWindow", "保存"))
         self.saveaction.setShortcut(_translate("MainWindow", "Ctrl+D"))
+        self.action12.setText(_translate("MainWindow", "查看帮助文档"))
+        self.actionas.setText(_translate("MainWindow", "关于"))
 
 
 if __name__ == "__main__":
