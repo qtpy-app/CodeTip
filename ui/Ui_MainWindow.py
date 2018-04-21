789e7a3b36001f00da5c387406731e234ffa80ee
Lin JH <625781186@qq.com>
2018-04-21 22:58:06 +0800

    主界面UI

---
+++ ui/Ui_MainWindow.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'D:\QGroup_432987409\Code_GitSyn_test\ui\MainWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(328, 321)
+        MainWindow.setTabShape(QtWidgets.QTabWidget.Rounded)
+        MainWindow.setDockOptions(QtWidgets.QMainWindow.AllowTabbedDocks|QtWidgets.QMainWindow.AnimatedDocks)
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralWidget)
+        self.gridLayout.setContentsMargins(4, 4, 4, 0)
+        self.gridLayout.setHorizontalSpacing(4)
+        self.gridLayout.setObjectName("gridLayout")
+        self.splitter = QtWidgets.QSplitter(self.centralWidget)
+        self.splitter.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter.setObjectName("splitter")
+        self.widget = QtWidgets.QWidget(self.splitter)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Ignored)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.widget.sizePolicy().hasHeightForWidth())
+        self.widget.setSizePolicy(sizePolicy)
+        self.widget.setObjectName("widget")
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.widget)
+        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.stackedWidget = QtWidgets.QStackedWidget(self.splitter)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Ignored)
+        sizePolicy.setHorizontalStretch(5)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.stackedWidget.sizePolicy().hasHeightForWidth())
+        self.stackedWidget.setSizePolicy(sizePolicy)
+        self.stackedWidget.setFrameShape(QtWidgets.QFrame.Panel)
+        self.stackedWidget.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.stackedWidget.setObjectName("stackedWidget")
+        self.page0 = QtWidgets.QWidget()
+        self.page0.setObjectName("page0")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.page0)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.stackedWidget.addWidget(self.page0)
+        self.page1 = QtWidgets.QWidget()
+        self.page1.setObjectName("page1")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.page1)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.stackedWidget.addWidget(self.page1)
+        self.gridLayout.addWidget(self.splitter, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralWidget)
+        self.toolBar = QtWidgets.QToolBar(MainWindow)
+        self.toolBar.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.toolBar.setOrientation(QtCore.Qt.Horizontal)
+        self.toolBar.setObjectName("toolBar")
+        MainWindow.addToolBar(QtCore.Qt.BottomToolBarArea, self.toolBar)
+        self.statusBar = QtWidgets.QStatusBar(MainWindow)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.statusBar.sizePolicy().hasHeightForWidth())
+        self.statusBar.setSizePolicy(sizePolicy)
+        self.statusBar.setMinimumSize(QtCore.QSize(0, 23))
+        self.statusBar.setSizeIncrement(QtCore.QSize(0, 23))
+        self.statusBar.setObjectName("statusBar")
+        MainWindow.setStatusBar(self.statusBar)
+        self.saveaction = QtWidgets.QAction(MainWindow)
+        self.saveaction.setObjectName("saveaction")
+        self.action12 = QtWidgets.QAction(MainWindow)
+        self.action12.setObjectName("action12")
+        self.actionas = QtWidgets.QAction(MainWindow)
+        self.actionas.setObjectName("actionas")
+        self.actiondz = QtWidgets.QAction(MainWindow)
+        self.actiondz.setObjectName("actiondz")
+        self.actiongc = QtWidgets.QAction(MainWindow)
+        self.actiongc.setObjectName("actiongc")
+        self.actiondb = QtWidgets.QAction(MainWindow)
+        self.actiondb.setObjectName("actiondb")
+
+        self.retranslateUi(MainWindow)
+        self.stackedWidget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
+        self.saveaction.setText(_translate("MainWindow", "保存"))
+        self.saveaction.setShortcut(_translate("MainWindow", "Ctrl+D"))
+        self.action12.setText(_translate("MainWindow", "查看帮助文档"))
+        self.actionas.setText(_translate("MainWindow", "关于"))
+        self.actiondz.setText(_translate("MainWindow", "dz"))
+        self.actiongc.setText(_translate("MainWindow", "gc"))
+        self.actiondb.setText(_translate("MainWindow", "db"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
